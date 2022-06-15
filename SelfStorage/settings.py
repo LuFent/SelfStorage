80dc813dae1ba41617ea9ada5bc180f81094406e
Roman Volodin <roman.volodin@gmail.com>
2022-06-15 16:54:27 +0300

    Add user logout functionality

--- SelfStorage/settings.py
+++ SelfStorage/settings.py
@@ -142,3 +142,4 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+LOGOUT_REDIRECT_URL = '/'
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
-from . import views
+from django.conf import settings
+from django.contrib.auth.views import LogoutView
 from django.urls import path
+
+from . import views
 
 
 app_name = 'users'
 
 urlpatterns = [
     path('login', views.login_user, name='login'),
+    path('logout', LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout')
 ]

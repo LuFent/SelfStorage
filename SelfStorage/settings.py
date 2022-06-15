f3bc2cc700fb803b1671b784d885fdca814438fd
Roman Volodin <roman.volodin@gmail.com>
2022-06-15 09:40:45 +0300

    Добавил кастомную модель пользователя

--- SelfStorage/settings.py
+++ SelfStorage/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'boxes',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,4 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'

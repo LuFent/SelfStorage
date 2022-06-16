a0193e96c80973a422b4715176786fd62193883b
Roman Volodin <roman.volodin@gmail.com>
2022-06-16 11:03:31 +0300

    Style user login form

--- users/forms.py
+++ users/forms.py
@@ -25,5 +25,18 @@ class CustomUserChangeForm(UserChangeForm):
 
 
 class LoginForm(forms.Form):
-    email = forms.CharField(widget=forms.EmailInput)
-    password = forms.CharField(widget=forms.PasswordInput)
+    classes = (
+        'form-control border-8 mb-4 py-3 px-5 border-0 fs_24 '
+        'SelfStorage__bg_lightgrey'
+    )
+
+    email = forms.CharField(
+        widget=forms.EmailInput(
+            attrs={'class': classes, 'placeholder': 'E-mail'}
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': classes, 'placeholder': 'Пароль'}
+        )
+    )

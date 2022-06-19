89567fde07272c48621a827325bc057246b09d42
949027 <82878668+949027@users.noreply.github.com>
2022-06-19 20:07:41 +0700

    Send code enabled

--- keystore/views.py
+++ keystore/views.py
@@ -1,34 +1,39 @@
 import os
+from random import randint
 
 import qrcode
 from django.core.mail import EmailMessage
 from django.shortcuts import redirect
+from django.urls import reverse
 
 from boxes.models import Order
-from SelfStorage.settings import EMAIL_HOST_USER
-
 from .models import BoxKey
+from SelfStorage.settings import EMAIL_HOST_USER
 
 
 def send_qr(request, order_id):
     order = Order.objects.get(id=order_id)
-    box_key = BoxKey.objects.get(order_id=order_id)
+    box_key, _ = BoxKey.objects.get_or_create(
+        order=order,
+        defaults={'code': randint(0, 99999)},
+    )
     filename = f"{order_id}.png"
-    img = qrcode.make(box_key)
+    img = qrcode.make(box_key.code)
     img.save(filename)
 
     text = (
         f"Здравствуйте, Ваш бокс под номером {order.box.number} "
-        f"находится на {order.box.floor} этаже. \nДля получения "
-        f"воспользуйтесь QR-кодом:"
+        f"находится на {order.box.floor} этаже по адресу {order.box.storage.address}."
+        f" \nДля получения воспользуйтесь QR-кодом:"
     )
     email = EmailMessage(
         "Доступ к боксу",
         text,
         EMAIL_HOST_USER,
-        ["949027@gmail.com"],  # TODO вставить емейл заказчика
+        [request.user.email],
     )
     email.attach_file(filename)
     email.send()
     os.remove(filename)
-    # return redirect(reverse('lk')) TODO тут будет редирект на личный кабинет
+
+    return redirect(reverse("users:account"))

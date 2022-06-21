a1b84a0c0a8ca9f838cd9b7485909ccb27ba5c91
949027 <82878668+949027@users.noreply.github.com>
2022-06-21 10:39:52 +0700

    Secret key moved to .env

--- SelfStorage/settings.py
+++ SelfStorage/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s-%fbgy=r#_6a@1by^(s(-#bo2g5f30e+n^851#^_!01+bx#6^"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
